419803b144e69e2432c718f148408f43d85e5494
dwqs <461147874@qq.com>
2018-06-26 15:34:03 +0800

    refactor: Replace html entities

--- src/index.js
+++ src/index.js
@@ -38,17 +38,20 @@ module.exports = function MpvueMarkdownParser(text) {
   if (!text || typeof text !== 'string') {
     return [];
   }
-  
+
   marked.setOptions({
     renderer: renderer,
     gfm: true,
     tables: true,
     breaks: true,
     pedantic: false,
-    sanitize: false,
+    sanitize: true,
     smartLists: true,
     smartypants: false
   });
 
-  return HTML2JSON(marked(text));
+  // 如果markdown中有html片段，会产生一些转义： https://github.com/markedjs/marked/issues/779
+  const htmlText = marked(text).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '');
+  
+  return HTML2JSON(htmlText);
 };
